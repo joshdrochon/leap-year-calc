1f5bc1decdf506b10bf43b2b006b2ad507cf4e49
Josh Rochon <joshdrochon@gmail.com>
2018-01-17 18:21:32 -0800

    second spec passed funct returns true

--- leap-year/js/scripts.js
+++ leap-year/js/scripts.js
@@ -5,10 +5,13 @@ $(document).ready(function(){
     var year = parseInt($("#userInput").val()); //note parseInt
     $("#initially-hidden").text(result); //displays returned boolean value
     var leapYear = function(year){
+      if(year % 4 === 0){ //if remainder(%) = 0 than divisible evals true
+        return true;
+      }else{
         return false;
+      }
     }
     var result = leapYear(year); //funct takes user input var as argument
-
     console.log(result);
 
   });
