0de674bd43ab7d23dd5ef8c02ce20f63902e6942
Josh Rochon <joshdrochon@gmail.com>
2018-01-17 18:40:36 -0800

    third spec passed funct returns false

--- leap-year/js/scripts.js
+++ leap-year/js/scripts.js
@@ -5,12 +5,15 @@ $(document).ready(function(){
     var year = parseInt($("#userInput").val()); //note parseInt
     $("#initially-hidden").text(result); //displays returned boolean value
     var leapYear = function(year){
-      if(year % 4 === 0){ //if remainder(%) = 0 than divisible evals true
+      if(year % 100 === 0){ //if remainder(%) = 0 than divisible evals true
+        return false;
+      }else if(year % 4 === 0){
         return true;
       }else{
         return false;
       }
-    }
+    };
+
     var result = leapYear(year); //funct takes user input var as argument
     console.log(result);
 
@@ -18,3 +21,5 @@ $(document).ready(function(){
 });
 
 //commit after each passing bahavior
+
+//when eval for x divisible by y use % op to check for 0
